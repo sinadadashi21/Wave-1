5d48a74f902864721d651fa7899bef9db180e1d4
Sina Dadashi <sina.dadashi21@ycdsbk12.ca>
2020-04-11 21:12:52 -0400

    Completed

---
+++ area-of-a-field.py
@@ -0,0 +1,5 @@
+length = float(input("What is the length of your field? (in ft) "))
+width = float(input("What is the width of your field? (in ft) "))
+
+area = length*width / 43560
+print("The area of the farm is " + str(area) + " acres")
