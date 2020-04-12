cddfd205ccdcc31c61cb1061a74530dc99426691
Sina Dadashi <sina.dadashi21@ycdsbk12.ca>
2020-04-12 00:50:48 -0400

    Completed

---
+++ volume-of-a-cylinder.py
@@ -0,0 +1,8 @@
+import math
+r = float(input("Enter the radius of the cylinder in metres: "))
+h = float(input("Enter the height of the cylinder in metres: "))
+area = math.pi * r**2
+volume = area * h
+volume = round(volume, 1)
+print("The volume of the cylinder is: " + str(volume) + "m")
+
