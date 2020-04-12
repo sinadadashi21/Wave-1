2fd41e1ff97617f5d37f9fbeff60b464a0d797c6
Sina Dadashi <sina.dadashi21@ycdsbk12.ca>
2020-04-12 01:22:52 -0400

    Corrected the unit of the volume of the cylinder.

--- volume-of-a-cylinder.py
+++ volume-of-a-cylinder.py
@@ -4,5 +4,5 @@ h = float(input("Enter the height of the cylinder in metres: "))
 area = math.pi * r**2
 volume = area * h
 volume = round(volume, 1)
-print("The volume of the cylinder is: " + str(volume) + "m")
+print("The volume of the cylinder is: " + str(volume) + " metres cubed")
 
